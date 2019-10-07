cf95af062263858d032118dbf3f8a9513a587e3a
Andyfoo <andyfoo@163.com>
2019-10-08 00:55:59 +0800

    update

--- 0_env_init.bat
+++ 0_env_init.bat
@@ -1,6 +1,6 @@
 go get github.com/akavel/rsrc
-go get github.com/lxn/walk
-go get github.com/lxn/win
+rem go get github.com/lxn/walk
+rem go get github.com/lxn/win
 
 go get github.com/GeertJohan/go.rice
 go get github.com/GeertJohan/go.rice/rice
@@ -10,7 +10,7 @@ go get golang.org/x/text
 go get github.com/gorilla/websocket
 
 
-go get github.com/dop251/goja
+rem go get github.com/dop251/goja
 
 
 
